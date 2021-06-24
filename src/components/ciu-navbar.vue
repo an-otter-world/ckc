932545d6d15a747936f9369b7270a587cb570a01
AnOtterWorld <59399648+an-otter-world@users.noreply.github.com>
2021-06-24 14:54:54 +0200

    Styled navbar.

--- src/components/ciu-navbar.vue
+++ src/components/ciu-navbar.vue
@@ -1,28 +1,53 @@
 <template lang="pug">
-div(class="ciu-flex-row ciu-primary-dark")
-  div(class=ciu-navbar-brand)
+div(class="ciu-flex-row ciu-primary ciu-navbar")
+  div(class="ciu-brand")
     slot(name="brand")
-  div(class=ciu-navbar-links)
+  div(class="ciu-links")
     slot
-  div(class=ciu-navbar-end)
+  div(class="ciu-end")
     slot(name="end")
 </template>
 
 <style>
-.ciu-navbar > .ciu-navbar-brand, .ciu-navbar-end {
-  flex-grow: 0;
+.ciu-navbar {
+  align-items: stretch;
 }
 
-.ciu-navbar > .ciu-navbar-main {
+.ciu-navbar > div {
+  display: flex;
+  align-items: center;
+}
+
+.ciu-navbar > .ciu-brand {
+  flex-grow: 0;
+  margin-left: var(--ciu-spacing);
+  margin-right: var(--ciu-spacing);
+}
+
+.ciu-navbar > .ciu-end {
+  flex-grow: 0;
+  margin-left: var(--ciu-spacing);
+  margin-right: var(--ciu-spacing);
+}
+
+.ciu-navbar > .ciu-links {
   flex-grow: 1;
 }
 
-.ciu-navbar > div > * {
-  margin-right: var(--ciu-control-spacing);
+.ciu-navbar > .ciu-links > a {
+  text-transform: uppercase;
+  align-items: center;
+  padding: var(--ciu-spacing);
 }
 
-.ciu-navbar a {
+.ciu-navbar a:hover {
+  background-color: var(--ciu-primary-light);
+}
+
+.ciu-navbar a.router-link-active {
+  background-color: var(--ciu-primary-dark);
   text-transform: uppercase;
 }
+
 </style>
 
