2e4d7669a02ad3926d9e9f1301f886e7cc90717f
AnOtterWorld <59399648+an-otter-world@users.noreply.github.com>
2021-06-24 15:05:58 +0200

    Added ciu-icon

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,30 @@ import CiuApiForm from './components/ciu-api-form.vue'
 import CiuApiInput from './components/ciu-api-input.vue'
 import CiuButton from './components/ciu-button.vue'
 import CiuComponent from './components/ciu-component.vue'
+import CiuIcon from './components/ciu-icon.vue'
 import CiuInput from './components/ciu-input.vue'
 import CiuLoadingOverlay from './components/ciu-loading-overlay.vue'
+import CiuNavbar from './components/ciu-navbar.vue'
 import CiuScreenCenter from './components/ciu-screen-center.vue'
 import CiuSpinner from './components/ciu-spinner.vue'
 import CiuTextField from './components/ciu-text-field.vue'
-import CiuNavbar from './components/ciu-navbar.vue'
 import { App } from 'vue';
 import { installResourceManager } from './core/resource-manager'
 import './assets/styles/_index.css'
+import './font-awesome'
 
 export default function install<T>(app: App<T>) {
     installResourceManager(app)
     app
       .component('CiuApiErrors', CiuApiErrors)
-      .component('CiuNavbar', CiuNavbar)
       .component('CiuApiForm', CiuApiForm)
       .component('CiuApiInput', CiuApiInput)
       .component('CiuButton', CiuButton)
       .component('CiuComponent', CiuComponent)
+      .component('CiuIcon', CiuIcon)
       .component('CiuInput', CiuInput)
       .component('CiuLoadingOverlay', CiuLoadingOverlay)
+      .component('CiuNavbar', CiuNavbar)
       .component('CiuScreenCenter', CiuScreenCenter)
       .component('CiuSpinner', CiuSpinner)
       .component('CiuTextField', CiuTextField)
